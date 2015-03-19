42b2ce3da3a397d75867d18332a7d67b9e0e5a32
drummeric <Wolfson.Eric@gmail.com>
2015-03-19 16:40:10 -0400

    Bullet Hell Start
    Basic movement and background

---
+++ BulletHell/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	private Rigidbody2D player;
+	private Vector2 vertical;
+	private Vector2 horizontal;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<Rigidbody2D> ();
+		vertical = new Vector2 (0, speed);
+		horizontal = new Vector2 (speed, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (Input.GetKey ("up") && screenPosition.y < 580) {
+			player.MovePosition (player.position + vertical);
+		} else if (Input.GetKey ("down") && screenPosition.y > 25) {
+			player.MovePosition(player.position - vertical);
+		} else if (Input.GetKey ("right") && screenPosition.x < 500) {
+			player.MovePosition(player.position + horizontal);
+		} else if (Input.GetKey ("left") && screenPosition.x > 25) {
+			player.MovePosition(player.position - horizontal);
+		}
+	}
+}
