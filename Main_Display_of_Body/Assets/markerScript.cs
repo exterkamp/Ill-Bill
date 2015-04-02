fa6d75dbe78e0eca13dca4fd9570ee69e9641410
Shane Exterkamp <gorefest5@gmail.com>
2015-04-01 23:21:48 -0400

    added markers to main body screen

---
+++ Main_Display_of_Body/Assets/markerScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class markerScript : MonoBehaviour {
+
+	public bool highlighted = false;
+
+	public string minigame;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && !highlighted) {
+			transform.localScale  = new Vector3(1F, 1F, 1F);
+		}
+
+
+
+	}
+
+	/*void OnMouseEnter() {
+		highlighted = true;
+	}*/
+
+	void OnMouseDown() {
+		//check if hit for a second time, if it is launch minigame!
+		if (highlighted) {
+			Application.LoadLevel (minigame);
+		}
+
+		highlighted = true;
+		transform.localScale  = new Vector3(1.25F, 1.25F, 1.25F);
+	}
+
+	void OnMouseExit(){
+		highlighted = false;
+	}
+
+}
