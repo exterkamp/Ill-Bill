2b200c5f0df91f04eda2fc53df5a84c2d1abe0d0
mburgunder <mitchellburgunder@yahoo.com>
2015-04-06 19:32:00 -0400

    remembered to add the marker script thingy

---
+++ Main_Display_of_Body/Assets/Scripts/markerScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+using System.Collections;
+
+public class markerScript : MonoBehaviour {
+
+	public bool highlighted = false;
+	public int difficulty;
+	public string minigame;
+	public GameObject bodyPoint;
+
+
+	public markerScript(int diff, string game, GameObject point){
+		this.difficulty = diff;
+		this.minigame = game;
+		this.bodyPoint = point;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0) && !highlighted) {
+			transform.localScale  = new Vector3(1F, 1F, 1F);
+		}
+
+
+
+	}
+
+	/*void OnMouseEnter() {
+		highlighted = true;
+	}*/
+
+	void OnMouseDown() {
+		//check if hit for a second time, if it is launch minigame!
+		if (highlighted) {
+			DictionaryMinigame.instance.setDiff(difficulty);
+			Application.LoadLevel (minigame);
+		}
+
+		highlighted = true;
+		transform.localScale  = new Vector3(1.25F, 1.25F, 1.25F);
+	}
+
+	void OnMouseExit(){
+		highlighted = false;
+	}
+
+}
