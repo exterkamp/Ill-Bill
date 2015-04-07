1c53b5c26bf6ee6d40a356ae7d5a1f5ac24a496a
Shane Exterkamp <exterkamps@ufl.edu>
2015-04-06 21:18:44 -0400

    add text to UI's

--- Main_Display_of_Body/Assets/Scripts/mapGraphController.cs
+++ Main_Display_of_Body/Assets/Scripts/mapGraphController.cs
@@ -5,12 +5,32 @@ using System.Collections.Generic;
 public class mapGraphController : MonoBehaviour {
 
 	public GameObject[] bodyPoints;
+	private string[][] bodyPointStrings = new string[16][]{  new string[]{"Headache","Jaw Fracture"},
+													        new string[]{"Heart Attack","Chest pain"},
+															new string[]{"Stomach Ache","Intestinal Distress"},
+															new string[]{"Broken Pelvis","Bruised Pelvis"},
+															new string[]{"Rolled Shoulder","Sprained Socket"},
+															new string[]{"Tennis Elbow","Broken Elbow"},
+															new string[]{"Bent Hand","Broken Hand"},
+															new string[]{"Rolled Shoulder","Sprained Socket"},
+															new string[]{"Tennis Elbow","Broken Elbow"},
+															new string[]{"Bent Hand","Broken Hand"},
+															new string[]{"Busted Hip","Sprained Hip"},
+															new string[]{"Bent Knee","Rotated Knee"},
+															new string[]{"Stubbed Toe","Broken Foot"},
+															new string[]{"Busted Hip","Sprained Hip"},
+															new string[]{"Bent Knee","Rotated Knee"},
+															new string[]{"Stubbed Toe","Broken Foot"}};
 	public GameObject markerPrefab;
 	public List<GameObject> markers;
 	private string[] levels = {"FlappyBirdMain", "missileCommandMain"};
+	private string[] levelString = {"Artery Runner", "Virus Defense"};
 	// Use this for initialization
+
+
 	public void generate(){
-		GameObject bP = bodyPoints[Random.Range(0,bodyPoints.Length)];
+		int bPindex = Random.Range (0, bodyPoints.Length-1);
+		GameObject bP = bodyPoints[bPindex];
 		GameObject marker = null;
 		for (int i = 0; i < markers.Count; i++) {
 			markerScript ms = markers[i].GetComponent<markerScript>();
@@ -20,10 +40,16 @@ public class mapGraphController : MonoBehaviour {
 			}
 		}
 		if (marker == null) {
-			string level = levels [Random.Range (0, levels.Length)];
+			int levelIndex = Random.Range (0, levels.Length);
+			string level = levels [levelIndex];
+			string levelText = levelString[levelIndex];
 			int diff = 1;
+			string[] bPTextFirst = bodyPointStrings[bPindex];
+			string bPText = bPTextFirst[Random.Range (0, bPTextFirst.Length-1)];
 			marker = (GameObject) Instantiate(markerPrefab, bP.transform.position, Quaternion.identity);
 			markerScript ms = marker.GetComponent<markerScript>();
+			ms.injury = bPText;
+			ms.minigameText = levelText;
 			ms.difficulty = diff;
 			ms.minigame = level;
 			ms.bodyPoint = bP;
