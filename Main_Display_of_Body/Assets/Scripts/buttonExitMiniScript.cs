e365d713a5c0ba468cfe83bb58089ba55c36117c
exterkamps <gorefest5@gmail.com>
2015-04-13 22:06:15 -0400

    bug fix

--- Main_Display_of_Body/Assets/Scripts/buttonExitMiniScript.cs
+++ Main_Display_of_Body/Assets/Scripts/buttonExitMiniScript.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class buttonExitMiniScript : MonoBehaviour {
 
 	public void onClickExitToMain(){
+		if (BodyMapController.instance != null) {
+			GameObject.Destroy(BodyMapController.instance.gameObject);
+			BodyMapController.instance = null;
+			DictionaryGameState.instance.setmarkers(new List<string[]>());
+		}
 		Application.LoadLevel ("MainMenu");
 	}
 }
