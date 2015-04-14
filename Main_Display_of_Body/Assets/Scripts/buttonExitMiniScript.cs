f4e98988e7d533a7a64f430754c2eaf75afb5c3e
exterkamps <gorefest5@gmail.com>
2015-04-13 20:22:39 -0400

    UI commit 1

---
+++ Main_Display_of_Body/Assets/Scripts/buttonExitMiniScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttonExitMiniScript : MonoBehaviour {
+
+	public void onClickExitToMain(){
+		Application.LoadLevel ("MainMenu");
+	}
+}
---
+++ Main_Display_of_Body/Assets/Scripts/mainMenuCameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainMenuCameraController : MonoBehaviour {
+
+	public void clickListenerPlay(){
+		//Debug.Log ("Play");
+		Application.LoadLevel ("BodyMap");
+	}
+
+	public void clickListenerOptions(){
+		Debug.Log ("Options");
+	}
+
+	public void clickListenerExit(){
+		Debug.Log ("Exit");
+	}
+
+}
