aeb4613bc44624c574d8b8f10acc5b17ff1a0544
zscott515 <zack.junk@live.com>
2015-04-16 15:47:32 -0400

    Win/Lose bug fix
    Continue game bug and new game bug resolved.

--- Main_Display_of_Body/Assets/Scripts/GamePromptScript.cs
+++ Main_Display_of_Body/Assets/Scripts/GamePromptScript.cs
@@ -9,87 +9,76 @@ public class GamePromptScript : MonoBehaviour {
 	public GameObject button;
 	public GameObject Panel;
 	//private string menu;
-
+	
 	public Text scoreText;
 	public Text resultText;
-
+	
 	private static int prevwins;
+	private static int prevlosses;
 	private static int prevscore;
 	private static int score;
 	private static int wins;
-
+	private static int losses;
+	
 	// Use this for initialization
 	void Start () {
 		//guiShow = true;
-
+		
 		if (instance == null) {
 			instance = this;
 			DontDestroyOnLoad (this.gameObject);
 		} else if(instance != this){
 			Destroy (this.gameObject);
 		}
-
-
+		
+		
 		if (instance != null) {
 			//wins = DictionaryMinigame.instance.getWins ();
 			//losses = DictionaryMinigame.instance.getLosses ();
-
-
-
-			if((wins = BodyMapController.instance.wins) !=0 || BodyMapController.instance.losses != 0){
-				guiShow = true;
-				if(prevwins<BodyMapController.instance.wins){
-				resultText.text = "You won !!!";
+			
+			
+			if(((wins = BodyMapController.instance.wins) !=0 || (losses = BodyMapController.instance.losses) != 0)   ){
+				
+				if(wins != prevwins|| losses != prevlosses){
+					guiShow = true;
+					
+					if(prevwins<BodyMapController.instance.wins){
+						resultText.text = "You won !!!";
+					}else{
+						resultText.text = "You lost :(";
+					}
+					
+					score=BodyMapController.instance.score;
+					scoreText.text = "You got a score of "+(score-prevscore);
+					
+					
+					prevwins = wins;
+					prevlosses = losses;
+					prevscore= score;
+					
 				}else{
-				resultText.text = "You lost :(";
+					guiShow = false;
 				}
-				score=BodyMapController.instance.score;
-				scoreText.text = "You got a score of "+(score-prevscore);
+				
 			}else{
 				guiShow = false;
+				prevwins = 0;
+				prevlosses = 0;
+				prevscore = 0;
 			}
-
-			prevwins = wins;
-			prevscore= score;
-		}//inst!=null
-
-
-
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	/*
-	void Awake(){
-		guiShow = true;
-		Debug.Log ("Access");
-
-		if (instance != null) {
-			wins = DictionaryMinigame.instance.getWins ();
-			losses = DictionaryMinigame.instance.getLosses ();
-			Debug.Log ("Access1");
-			if(losses !=0 && wins != 0){
-				
-				guiShow = true;}else{guiShow = true;}
+			
 			
 		}//inst!=null
-
+		
+		
+		
+		
 	}
-*/
+
 	public void noGUI(){
 		guiShow = false;
 	}
-	
-	public void showGUI(string scene, string injuryText, string minigameText, int difficultyText){
-		guiShow = true;
-		//menu = scene;
-		/*injury.GetComponent<Text> ().text = injuryText;
-		minigame.GetComponent<Text> ().text = minigameText;
-		Difficulty.GetComponent<Text> ().text = "Difficulty: " + difficultyText;*/
-	}
+
 	
 	void OnGUI(){
 		if (guiShow) {
