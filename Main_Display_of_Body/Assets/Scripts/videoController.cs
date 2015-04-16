c9e786bd3154ef09fc78de5d9efeb58d6991263c
Shane Exterkamp <exterkamps@ufl.edu>
2015-04-16 02:16:36 -0400

    small changes and addition of video

---
+++ Main_Display_of_Body/Assets/Scripts/videoController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class videoController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Handheld.PlayFullScreenMovie ("main_menu_bg_video.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
